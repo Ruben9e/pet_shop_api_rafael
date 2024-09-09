073c07ea4c7ee2dd6deb582fe7ec4d7cd673ef2c
Ann Castro <anncasstro@gmail.com>
2024-09-09 03:27:24 -0500

    Configuracion Inicial

---
+++ src/test/java/com/pet_shop/pet_shop_api/PetShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet_shop.pet_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
